15595b1746ba6fd265e4aaca2d407d615354cce5
Lawful2002 <sharshvardhan101@gmail.com>
2021-12-26 21:02:11 +0530

    updated UI

--- src/components/layouts/TopNav.vue
+++ src/components/layouts/TopNav.vue
@@ -1,33 +1,73 @@
 <template>
   <div class="topnav d-flex align-items-center justify-content-between">
-    <select
-      v-on:change="langChange"
-      class="form-select m-2"
-      aria-label="Select language"
-      ref="lang"
-    >
-      <option value="cpp" selected>C++</option>
-      <option value="javascript">Javascript</option>
-      <option value="python">Python</option>
-      <option value="java">Java</option>
-      <option value="csharp">C#</option>
-    </select>
-    <b-dropdown
-      id="dropdown-1"
-      text="More Options"
-      class="m-2"
-      variant="primary"
-    >
-      <b-dropdown-item disabled>Run Code</b-dropdown-item>
-      <b-dropdown-item disabled>Upload from File</b-dropdown-item>
-      <b-dropdown-item disabled>Download Code</b-dropdown-item>
-      <b-dropdown-item disabled>Collab Mode</b-dropdown-item>
-      <b-dropdown-item @click="clearStore">Clear Cache</b-dropdown-item>
-    </b-dropdown>
+    <div>
+      <select
+        v-on:change="langChange"
+        class="form-select m-2"
+        aria-label="Select language"
+        ref="lang"
+      >
+        <option value="cpp" selected>C++</option>
+        <option value="javascript">Javascript</option>
+        <option value="python">Python</option>
+        <option value="java">Java</option>
+        <option value="csharp">C#</option>
+      </select>
+    </div>
+
+    <div class="d-flex justify-content-center align-items-center">
+      <b-icon
+        title="Run"
+        class="round mx-2"
+        icon="play-circle"
+        variant="secondary"
+      ></b-icon>
+      <b-icon
+        title="Upload Code"
+        class="round mx-2"
+        icon="file-earmark-arrow-up"
+        variant="secondary"
+      ></b-icon>
+      <b-icon
+        title="Download Code"
+        class="round mx-2"
+        icon="file-earmark-arrow-down"
+        variant="secondary"
+      ></b-icon>
+      <b-icon
+        title="Colab Mode"
+        class="round mx-2"
+        icon="people-fill"
+        variant="secondary"
+      ></b-icon>
+      <b-icon
+        title="Clear Cache"
+        class="round mx-2"
+        icon="trash"
+        variant="danger"
+        @click="clearStore"
+      ></b-icon>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.round {
+  height: 38px;
+  width: 38px;
+}
+.round:nth-child(1):hover {
+  color: green;
+}
+.round:nth-child(2):hover {
+  color: yellow;
+}
+.round:nth-child(3):hover {
+  color: yellow;
+}
+.round:nth-child(5):hover {
+  color: red;
+}
 .topnav {
   background-color: #003367;
   padding: 20px;
@@ -36,10 +76,7 @@
   border-radius: 20px;
 }
 .form-select {
-  width: 150px;
-}
-.action-buttons {
-  width: 35%;
+  min-width: 150px;
 }
 </style>
 
@@ -58,6 +95,7 @@ export default {
   methods: {
     clearStore() {
       localStorage.clear();
+      console.log('cache cleared')
     },
     langChange() {
       let language = this.$refs.lang.value;
